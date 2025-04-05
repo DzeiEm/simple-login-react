bf1398a48dddf24c45dabf8daeb833d22363701e
DzeiEm <julija.melesina@gmail.com>
2025-04-05 18:34:57 +0300

    add diff approach of styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+// import "./Header.css";
+import ccsClasses from "./Header.module.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className={ccsClasses.paragraph}>
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
